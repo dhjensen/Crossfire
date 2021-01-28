acfa929412d9256928c53d5b3f1b555f6bb357f3
Daniel Jensen <dhjen@outlook.com>
2021-01-28 19:05:35 +0100

    Improved the script

--- checkheight.sh
+++ checkheight.sh
@@ -1,4 +1,5 @@
-echo "Chain height:"
-curl -s https://crossfire.crypto.com/commit | jq "{height: .result.signed_header.header.height}"
-echo -e  "\nLocal height:"
-docker exec -i crossfire-docker_crossfire_1 curl -s http://127.0.0.1:26657/commit | jq '{height: .result.signed_header.header.height}'
+chainheight=$(curl -s https://crossfire.crypto.com/commit | jq -r '.result.signed_header.header.height')
+localheight=$(docker exec -i crossfire-docker_crossfire_1 curl -s http://127.0.0.1:26657/commit | jq -r '.result.signed_header.header.height')
+
+echo "Chain height: $chainheight"
+echo "Local height: $localheight"
