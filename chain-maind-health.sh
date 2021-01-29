c6246cb5cc0b19248f7f5d5f773c870436e13df5
Daniel Jensen <dhjen@outlook.com>
2021-01-29 17:15:53 +0100

    Added health script

---
+++ chain-maind-health.sh
@@ -0,0 +1,20 @@
+#!/bin/bash --
+
+docker_dir="$HOME/Projects/docker/crossfire-docker/"
+pid=$(docker inspect -f '{{.State.Pid}}' crossfire-docker_crossfire_1)
+echo $pid
+mem_usage_percent=$(ps h -o %mem $pid)
+#echo $mem_usage_percent
+mem_limit_percent=35
+#echo $mem_limit_percent
+#echo "$mem_usage_percent > $mem_limit_percent"|bc -l
+
+if [ $(echo "$mem_usage_percent > $mem_limit_percent"|bc -l) -eq 1 ]; then
+    echo "Memory $mem_usage_percent>$mem_limit_percent - Recreate docker container"
+    cd $docker_dir
+    docker-compose down
+    docker-compose up -d
+else
+    echo "Memory $mem_usage_percent<$mem_limit_percent - Don't restart"
+
+fi
