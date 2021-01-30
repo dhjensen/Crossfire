c4275cceda93a7b905057732409d04bf8f9ac3b0
Daniel Jensen <dhjen@outlook.com>
2021-01-30 13:16:20 +0100

    Added height difference

--- checkheight.sh
+++ checkheight.sh
@@ -1,5 +1,7 @@
 chainheight=$(curl -s https://crossfire.crypto.com/commit | jq -r '.result.signed_header.header.height')
 localheight=$(docker exec -i crossfire-docker_crossfire_1 curl -s http://127.0.0.1:26657/commit | jq -r '.result.signed_header.header.height')
+difference=$(expr $chainheight - $localheight)
 
 echo "Chain height: $chainheight"
 echo "Local height: $localheight"
+echo "Height difference: $difference"
